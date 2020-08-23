a9a93609cd7ab9e1fc833f67fc414d00e270ba51
Uanderson Leonel <uandzleonel@outlook.com>
2020-08-22 21:38:32 -0400

    New about description

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ server.get('/', (request, response) => {
     avatar_url: "https://avatars1.githubusercontent.com/u/16707128?s=460&u=3ce3d7fe5b0cbbfd2f22f4c78869a00c96a9bda9&v=4",
     name: "Uanderson Leonel",
     title: "Aprendendo Frontend e Backend",
-    description: 'Graduado em Ciência da Computação e desenvolvendo em DataFlex desde 2015. É DBA em SQL Server pelo MSL. No momento é Coordenador de desenvolvimento na <a class="links" href="https://kpers.com.br/" target="_blank">KPERS</a>.',
+    description: 'Graduado em Ciência da Computação e desenvolvendo em DataFlex desde 2015. É DBA em SQL Server 2017® pelo Microsoft Silver Learning. No momento é Coordenador de desenvolvimento na <a class="links" href="https://kpers.com.br/" target="_blank">KPERS</a>.',
     links: [
       { name: "KPERS", url: "https://kpers.com.br/" }
     ]
